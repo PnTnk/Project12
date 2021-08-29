23dcb023dd63d2a11c3800790320e283df74d385
mrpan <mrpan@LAPTOP-ITM26DLA>
2021-08-29 23:29:29 +0700

    Add project files.

---
+++ Project12/Source.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("Enter number :");
+	scanf_s("%d", &n);
+	int x = 1;
+	while (x <= n) {
+		int a = 1;
+		while (a <= 2 * n) {
+			if (a < x || a>2 * n - x) {
+				printf(" ");
+				a += 1;
+			}
+			else {
+				printf("*");
+				a += 1;
+			}
+		}
+		x += 1;
+		printf("\n");
+	}
+	int y = 1;
+	while (y < n) {
+		int b = 1;
+		while (b < 2 * n) {
+			if (b < n - y||b > n+y) {
+				printf(" ");
+				b += 1;
+			}
+			else {
+				printf("*");
+				b += 1;
+			}
+		}
+		y += 1;
+		printf("\n");
+	}
+
+	return 0;
+}
